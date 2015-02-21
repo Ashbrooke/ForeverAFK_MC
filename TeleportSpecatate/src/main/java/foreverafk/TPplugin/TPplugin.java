856f6b76f00ece13f37f4ced3412f8b9b4fc3394
aostia <aostia@uwo.ca>
2015-02-20 21:20:42 -0500

    Basic Project

---
+++ TeleportSpecatate/src/main/java/foreverafk/TPplugin/TPplugin.java
@@ -0,0 +1,26 @@
+package foreverafk.TPplugin;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
+public class TPplugin extends JavaPlugin{
+	
+    
+	public void onEnable() {
+    	// TODO Insert logic to be performed when the plugin is enabled
+
+	}
+
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
